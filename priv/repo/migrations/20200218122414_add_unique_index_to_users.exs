8bc57602575b3dcc804035f73728d725e5904e44
pojiro <pojiro.jp@gmail.com>
2020-02-19 14:02:43 +0900

    Don't use citext extension, cause gigalixir Free Tier does not support postgres extension

--- priv/repo/migrations/20200218122414_add_unique_index_to_users.exs
+++ priv/repo/migrations/20200218122414_add_unique_index_to_users.exs
@@ -1,23 +1,7 @@
 defmodule SampleApp.Repo.Migrations.AddUniqueIndexToUsers do
   use Ecto.Migration
 
-  def up do
-    execute "CREATE EXTENSION IF NOT EXISTS citext"
-
-    alter table("users") do
-      modify :email, :citext
-    end
-
+  def change do
     create unique_index(:users, [:email])
-  end
-
-  def down do
-    drop unique_index(:users, [:email])
-
-    alter table("users") do
-      modify :email, :string
-    end
-
-    execute "DROP EXTENSION IF EXISTS citext"
   end
 end
